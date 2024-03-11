ae6949976597317e51aef5d4a2d159692a2c6ed3
Courtney Lawson <courtneyalawson@yahoo.com>
2024-03-11 16:22:21 -0400

    logger-and-authentication-added

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ require('dotenv').config();
 const Express = require('express');
 const app = Express();
 
+
 const port = process.env.PORT || 3004;
 
 // Import routes
 const HealthRoutes = require('./src/routes/health.routes');
 
+// logger middleware
+app.use((req, res, next) => {
+    const method = req.method; // GET, POST, etc.
+    const url = req.originalUrl; // API called
+    const dateTime = new Date().toISOString(); // current date and time
+
+    console.log(`Method: ${method}, API: ${url}, Date and Time: ${dateTime}`);
+
+    next(); // pass to next middleware function
+});
+
+// Authentication middleware
+const authenticateToken = require('./authMiddleware'); // Adjust the path as necessary
+
+// Apply authentication middleware to routes that require it
+// For example, if you have a route like /protected-route, apply the middleware like this:
+// app.get('/protected-route', authenticateToken, (req, res) => { ... });
 
 app.use(Express.json());
 
